3a6d530ede2f4dc446310a448a02e8db11c8ee57
Marek <marcosxd44@gmail.com>
2019-05-08 20:14:32 +0200

    Fix mistakes. Working for user defined number of matrices

--- Matrix Multiplication Problem/main.cpp
+++ Matrix Multiplication Problem/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream> 
 #include<iomanip> 
 #include<cstdio> 
+#include <string>
 using namespace std; 
 int counter = 0;
 int** Create2dimArray(int n);
+void DeleteArray(int** arr, int n);
 void print_matrix(int **arr , int n)
 { 
 	for(int i = 0 ; i< n ; i++)
@@ -15,36 +17,37 @@ void print_matrix(int **arr , int n)
 	cout << endl; 
 	} 
 } 
-char ccc[50]; 
+string ccc; 
 int cc = 0; 
 void print(int **s,int l,int h) 
 { 
 	if(s[l][h]!=0) 
 	{ 
-		printf("(parenthesis[%d][%d] = %d) != 0 " , l,h ,s[l][h]);
-		cout << " , then print ( and call function again\n";
+		//printf("(parenthesis[%d][%d] = %d) != 0 " , l,h ,s[l][h]);
+		//cout << " , then print ( and call function again\n";
 		///cout<<"("; 
-		ccc[cc] = '(';
-		cc++; print(s,l,s[l][h]);
+		ccc += "(";
+		cc++; 
+		print(s,l,s[l][h]);
 	} 
 	if(s[l][h]==0)
 	{
 		///cout << "A" << ++counter;
 		++counter;
-		ccc[cc] = 'A';
+		ccc+= "A";
 		cc++;
-		ccc[cc] = counter + '0';
+		ccc+= to_string(counter);
 		cc++; 
-		printf("(parenthesis[%d][%d] = %d) == 0 " ,l,h , s[l][h]);
-		cout << " , then print matrix " << "A" << counter << endl;
+		//printf("(parenthesis[%d][%d] = %d) == 0 " ,l,h , s[l][h]);
+		//cout << " , then print matrix " << "A" << counter << endl;
 	} 
 	if(s[l][h]!=0) 
 	{
 		print(s,s[l][h]+1,h);
-		printf("(parenthesis[%d][%d] = %d) != 0 ", l,h , s[l][h]); 
-		cout << " ,call function again and then print ) \n";
+		//printf("(parenthesis[%d][%d] = %d) != 0 ", l,h , s[l][h]); 
+		//cout << " ,call function again and then print ) \n";
 		///cout<<")";
-		ccc[cc] = ')'; 
+		ccc+= ")"; 
 		cc++; 
 	} 
 } 
@@ -53,7 +56,7 @@ int main()
 	int n, i;
 	cout << "Enter how many matrices do you want to have: ";
 	cin >> n;
-	int p[] = { 4,3,8,5,12,2,4,3,8,3};
+	int p[] = { 4,3,8,5,12,2,4,3,8,4,9,3,5,3,4,2,3};
 	int** m = Create2dimArray(n);
 	int** s = Create2dimArray(n);
 	int j, q;
@@ -65,31 +68,34 @@ int main()
 	{
 		for (int i = 1; i < n - d; i++)
 		{
-			j = i + d; m[i][j] = 9999;
-			cout << "storing minimum total multiplications from matrix " << i << " to matrix " << j << endl << endl;
+			j = i + d; 
+			m[i][j] = 9999;
+			//cout << "storing minimum total multiplications from matrix " << i << " to matrix " << j << endl << endl;
 			for (int k = i; k <= j - 1; k++)
 			{
 				q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-				printf("total multiplication = cost_matrix[%d][%d] + cost_matrix[%d][%d] + matrix_size[%d] * matrix_size[%d] * matrix_size[%d] = %d + %d + %d*%d*%d = %d\n", i, k, k + 1, j, i - 1, k, j, m[i][k], m[k + 1][j], p[i - 1], p[k], p[j], q);
+				//printf("total multiplication = cost_matrix[%d][%d] + cost_matrix[%d][%d] + matrix_size[%d] * matrix_size[%d] * matrix_size[%d] = %d + %d + %d*%d*%d = %d\n", i, k, k + 1, j, i - 1, k, j, m[i][k], m[k + 1][j], p[i - 1], p[k], p[j], q);
 				if (q < m[i][j]) {
 					m[i][j] = q; s[i][j] = k;
 				}
 			}
-			printf(" ==> minimum total multiplication ( cost_matrix[%d][%d] ) = %d\n", i, j, m[i][j]); cout << endl;
-			print_matrix(m, n);
-			printf("\nmatrix for parenthesis ( parenthesis[%d][%d] ) = %d\n", i, j, s[i][j]);
-			print_matrix(s, n);
-			cout << endl << endl << endl << endl << endl << endl << endl << endl;
+			//printf(" ==> minimum total multiplication ( cost_matrix[%d][%d] ) = %d\n", i, j, m[i][j]); cout << endl;
+			//print_matrix(m, n);
+			//printf("\nmatrix for parenthesis ( parenthesis[%d][%d] ) = %d\n", i, j, s[i][j]);
+			//print_matrix(s, n);
+			//cout << endl << endl << endl << endl << endl << endl << endl << endl;
 		}
 	}
-	cout << "TOTAL MULTIPLICATIONS : " << m[1][n - 1] << " \n\n";
-	cout << "\n\n\n\nPARENTHESIS : \n";
+	//cout << "TOTAL MULTIPLICATIONS : " << m[1][n - 1] << " \n\n";
+	//cout << "\n\n\n\nPARENTHESIS : \n";
 	print(s, 1, n-1);
 	cout << endl << endl;
 	cout << "\nFinal Parenthesis : ";
-	for (int i = 0; i < cc; i++)
-		cout << ccc[i];
+	//for (int i = 0; i < cc; i++)
+		cout << ccc;
 	cout << endl << endl << endl << endl << endl;
+	DeleteArray(m, n);
+	DeleteArray(s, n);
 }
 int **Create2dimArray(int n)
 {
@@ -100,4 +106,11 @@ int **Create2dimArray(int n)
 		for (int j = 0; j < n; j++)
 			arr[i][j] = 0;
 	return arr;
+}
+void DeleteArray(int** arr, int n)
+{
+	for (int i = 0; i < n; ++i) {
+		delete[] arr[i];
+	}
+	delete[] arr;
 }
